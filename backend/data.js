9be45f67dcfd6e3f4691806310363a6db5a06922
Rukshan123 <rukzmadu@gmail.com>
2021-11-26 11:40:51 +0530

    create dummy todos and check it on api

---
+++ backend/data.js
@@ -0,0 +1,15 @@
+const data = [
+  {
+    _id: '1',
+    title: 'todo01',
+    state: 'not completed',
+    endDate: '2021/12/10',
+  },
+  {
+    _id: '2',
+    title: 'todo02',
+    state: 'not completed',
+    endDate: '2021/12/25',
+  },
+]
+module.exports = data
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,20 @@
 const express = require('express')
+const data = require('./data.js')
 
 const app = express()
 const port = 5000
+
+app.get('/', (req, res) => {
+  res.send('Server is ready')
+})
+
+app.get('/api/todos', (req, res) => {
+  res.json(data)
+})
+
+app.get('/api/todos/:id', (req, res) => {
+  const todo = data.find((id) => id._id === req.params.id)
+  res.send(todo)
+})
 
 app.listen(port, console.log(`server started on PORT ${port}`))
