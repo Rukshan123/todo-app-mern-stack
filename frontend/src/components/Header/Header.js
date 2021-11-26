04533b9d9a5b85491216829b2866c616f5d4854f
Rukshan123 <rukzmadu@gmail.com>
2021-11-26 12:31:14 +0530

    implement header

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,5 +1,17 @@
 import React from 'react'
+import { Navbar, Container, Nav } from 'react-bootstrap'
 
 export default function Header() {
-  return <div>Header</div>
+  return (
+    <div>
+      <Navbar bg='primary' variant='dark'>
+        <Container>
+          <Navbar.Brand href='#home'>Todo App</Navbar.Brand>
+          <Nav className='me-auto'>
+            <Nav.Link href='#home'>Home</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  )
 }
