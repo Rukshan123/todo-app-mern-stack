cb853730298d1acfc562b1545f61ce5155f92df4
Rukshan123 <rukzmadu@gmail.com>
2021-11-26 21:39:39 +0530

    implement getTodoById update  and delete todo api

--- backend/controller/todoController.js
+++ backend/controller/todoController.js
@@ -18,4 +18,45 @@ const createTodo = asyncHandler(async (req, res) => {
   }
 })
 
-module.exports = { getTodos, createTodo }
+const getTodoById = asyncHandler(async (req, res) => {
+  const todo = await Todos.findById(req.params.id)
+
+  if (todo) {
+    res.json(todo)
+  } else {
+    res.status(404).json({ message: 'Todo not found' })
+  }
+
+  // res.json(todo)
+})
+
+const updateTodo = asyncHandler(async (req, res) => {
+  const { title, endDate } = req.body
+
+  const todo = await Todos.findById(req.params.id)
+
+  if (todo) {
+    todo.title = title
+    todo.endDate = endDate
+
+    const updateTodo = await todo.save()
+    res.json(updateTodo)
+  } else {
+    res.status(404)
+    throw new Error('Todo not found')
+  }
+})
+
+const DeleteTodo = asyncHandler(async (req, res) => {
+  const todo = await Todos.findById(req.params.id)
+
+  if (todo) {
+    await todo.remove()
+    res.json({ message: 'todo Removed' })
+  } else {
+    res.status(404)
+    throw new Error('todo not Found')
+  }
+})
+
+module.exports = { getTodos, createTodo, getTodoById, updateTodo, DeleteTodo }
--- backend/routes/todoRoutes.js
+++ backend/routes/todoRoutes.js
@@ -1,9 +1,15 @@
 const express = require('express')
-const { getTodos, createTodo } = require('../controller/todoController')
+const {
+  getTodos,
+  createTodo,
+  getTodoById,
+  updateTodo,
+  DeleteTodo,
+} = require('../controller/todoController')
 const router = express.Router()
 
 router.route('/').get(getTodos)
 router.route('/create').post(createTodo)
-// router.route('/:id').get().put().delete()
+router.route('/:id').get(getTodoById).put(updateTodo).delete(DeleteTodo)
 
 module.exports = router
