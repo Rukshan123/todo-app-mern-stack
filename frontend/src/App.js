9ddf78d0d91bcf298e90cb7362e155da2a913877
Rukshan123 <rukzmadu@gmail.com>
2021-11-26 13:57:01 +0530

    use routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,20 @@
 import './App.css'
 import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Todos from './screens/Todos.js'
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <main style={{ minHeight: '90vh' }}></main>
+      <main>
+        <Routes>
+          <Route path='/' element={<Todos />}></Route>
+        </Routes>
+      </main>
       <Footer />
-    </>
+    </BrowserRouter>
   )
 }
 
